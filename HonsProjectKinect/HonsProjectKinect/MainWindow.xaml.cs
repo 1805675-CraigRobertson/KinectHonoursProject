8bd756f0027564f29616adcd1098e73efb094971
1805675-CraigRobertson <1805675@rgu.ac.uk>
2020-02-02 18:37:26 +0000

    Added variables needed

--- HonsProjectKinect/HonsProjectKinect/MainWindow.xaml.cs
+++ HonsProjectKinect/HonsProjectKinect/MainWindow.xaml.cs
@@ -16,11 +16,32 @@ using Microsoft.Kinect;
 
 namespace HonsProjectKinect
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private const double HandSize = 30;
+        private const double JointThickness = 3;
+        private const double ClipBoundsThickness = 10;
+        private const float InferredZPositionClamp = 0.1f;
+        private readonly Brush handClosedBrush = new SolidColorBrush(Color.FromArgb(128, 255, 0, 0));
+        private readonly Brush handOpenBrush = new SolidColorBrush(Color.FromArgb(128, 0, 255, 0));
+        private readonly Brush handLassoBrush = new SolidColorBrush(Color.FromArgb(128, 0, 0, 255));
+        private readonly Brush trackedJointBrush = new SolidColorBrush(Color.FromArgb(255, 68, 192, 68));       
+        private readonly Brush inferredJointBrush = Brushes.Yellow;       
+        private readonly Pen inferredBonePen = new Pen(Brushes.Gray, 1);
+        private DrawingGroup drawingGroup;
+        private DrawingImage imageSource;
+        private KinectSensor kinectSensor = null;
+        private CoordinateMapper coordinateMapper = null;
+        private BodyFrameReader bodyFrameReader = null;
+        private Body[] bodies = null;
+        private List<Tuple<JointType, JointType>> bones;
+        private int displayWidth;
+        private int displayHeight;
+        private List<Pen> bodyColors;
+        private string statusText = null;
+
+
+
         public MainWindow()
         {
             InitializeComponent();
